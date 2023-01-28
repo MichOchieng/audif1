2932a6ea23c78ce3dd225421bcd2d825f3d45a3d
MichOchieng <MichOchieng@gmail.com>
2023-01-28 11:14:21 -0800

    Removed boiler plate code

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Audi F1 Team</title>
+        <meta name="description" content="Sauber to become Audi works F1 team from 2026" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className=''>
+       
+      </main>
+    </>
+  )
+}
