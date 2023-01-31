6bf8b5702504e47f0db567dc4887c8208fcfd6a7
MichOchieng <MichOchieng@gmail.com>
2023-01-31 14:58:51 -0800

    Added remaining card data

--- components/cardData.ts
+++ components/cardData.ts
@@ -1,9 +1,21 @@
-import img1 from '../public/carImgs/006.jpg'
+import img1 from '../public/carImgs/card0.jpg'
+import img2 from '../public/carImgs/card1.jpg'
+import img3 from '../public/carImgs/card2.jpg'
 
 export default [
     {
         h1: 'Innovation',
         h2: 'Entering the highest class of motorsports',
         bg: img1
-    }
+    },
+    {
+        h1: 'Sustainability',
+        h2: 'Sustainable synthetic fuels from 2026',
+        bg: img2
+    },
+    {
+        h1: 'Performance',
+        h2: 'Driving the  world’s most important motorsports series',
+        bg: img3
+    },
 ]
