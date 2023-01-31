121e5f7a1b3022c5e6d6d59e2d93f8480bfd9e6a
MichOchieng <MichOchieng@gmail.com>
2023-01-31 15:21:34 -0800

    Added header, fixed image positioning

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import img from '../public/carImgs/main.jpg'
+import logo from '../public/audiLogo.png'
 const Hero = () => {
     return (
-        <div>
+        <div className='relative flex flex-col items-center justify-center'>
             {/* Bg img */}
-            <div className='h-screen w-[400px] flex z-0'>
+            <div className='h-screen min-h-[800px] w-[400px] flex z-0'>
                 <Image
                     className='object-cover'
                     src={img}
@@ -13,8 +14,9 @@ const Hero = () => {
                 />
             </div>
             {/* Header */}
-            <div>
-                
+            <div className='absolute flex flex-col items-center justify-center top-0 border w-full h-72 z-10'>
+                <Image className='h-24 w-44' src={logo} alt='logo'/>
+                <h1 className="">Audi Enters Formula 1</h1>
             </div>
         </div>
     )
