c8f68c2786bfe46e2583e5ce6cb2dd5ccddce1e0
Mich Ochieng <michochieng@gmail.com>
2023-01-30 20:34:37 -0800

    First commit

---
+++ components/cardData.ts
@@ -0,0 +1,9 @@
+import img1 from '../public/carImgs/006.jpg'
+
+export default [
+    {
+        h1: 'Innovation',
+        h2: 'Entering the highest class of motorsports',
+        bg: img1
+    }
+]
