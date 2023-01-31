c3ba888bbd6c97130f713c07912998ec778e4ba5
MichOchieng <MichOchieng@gmail.com>
2023-01-31 14:56:18 -0800

    Added parallax layers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import { Card } from '@/components'
+import { Card, Hero } from '@/components'
 import cardData from '@/components/cardData'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import logo from '../public/audiLogo.png'
 
 export default function Home() {
   return (
@@ -14,7 +15,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=''>
-       <Card h1={cardData[0].h1} h2={cardData[0].h2} bg={cardData[0].bg}/>
+        <Parallax pages={3}>
+          <ParallaxLayer offset={0} sticky={{ start:0, end:0.2 }} speed={0.5}>
+            <Hero />
+          </ParallaxLayer>
+          <ParallaxLayer sticky={{ start: 1, end: 2.5 }} speed={0.5} >
+            <Card h1={cardData[0].h1} h2={cardData[0].h2} bg={cardData[0].bg} />
+          </ParallaxLayer>
+          <ParallaxLayer sticky={{ start: 1.54, end: 2.5 }} speed={0.5} >
+            <Card h1={cardData[1].h1} h2={cardData[1].h2} bg={cardData[1].bg} />
+          </ParallaxLayer>
+          <ParallaxLayer sticky={{ start: 2.08, end: 2.5 }} speed={0.5} >
+            <Card h1={cardData[2].h1} h2={cardData[2].h2} bg={cardData[2].bg} />
+            <div className='h-[360px] -mt-5 bg-[#161616] rounded-b-3xl relative flex flex-row items-end justify-center'>
+            <Image className='bottom-0 h-24 w-44' src={logo} alt='logo'/>
+            </div>
+          </ParallaxLayer>
+        </Parallax>
       </main>
     </>
   )
