d23d29a9e72ab40978bd8b24476be9ef1f336694
MichOchieng <MichOchieng@gmail.com>
2023-02-04 15:14:41 -0800

    Added slide in animation to tile text

--- pages/innovation.tsx
+++ pages/innovation.tsx
@@ -26,34 +26,40 @@ export default function Innovation() {
             </Head>
             <main className='page-container'>
                 {/* Header img */}
-                <div className="h-72 relative">
+                <div className="h-72 relative md:h-96 w-screen">
                     <Image
                         className='object-cover h-full z-0'
                         src={headerImg}
                         alt='bg'
                     />
-                    <InView threshold={0} triggerOnce>
+                    <InView threshold={0} >
                         {({ inView, ref }) => (
                             <div ref={ref} className={`bg-black/30 absolute top-0 h-full w-full flex items-center justify-center ${inView ? 'opacity-100' : 'opacity-0'} transition-all delay-100 duration-1000`}>
                                 <div className={`overflow-hidden relative h-10 w-full flex items-center justify-center`}>
-                                    <h1 className={`font-formulaWide absolute ${inView ? 'mt-0' : 'mt-14'} delay-500 duration-1000`}>Innovation</h1>
+                                    <h1 className={`font-formulaWide absolute ${inView ? 'mt-0' : 'mt-14'} delay-500 duration-1000 md:text-xl`}>Innovation</h1>
                                 </div>
                             </div>
                         )}
                     </InView>
                 </div>
                 {/* header */}
-                <div className="p-5 text-center">
-                    <h1 className="font-formulaWide">Actively Shaping Formula 1&apos;s Transformation</h1>
+                <div className="p-5 text-center md:my-5">
+                    <h1 className="font-formulaWide md:text-xl">Actively Shaping Formula 1&apos;s Transformation</h1>
                 </div>
                 {/* Block 1 */}
-                <div className="flex flex-col md:flex-row w-full p-5 my-3">
+                <div className="flex flex-col items-center md:flex-row w-full p-5 my-3">
                     {/* Tile */}
                     <Tile img={tileImg} />
                     {/* Text */}
-                    <p className="font-formulaRegular my-5">
-                        In view of the major technological leaps that the series is making towards sustainability in 2026, we can speak of a new Formula 1. Formula 1 is transforming, and Audi wants to actively support this journey. A close link between our Formula 1 project and AUDI AG&apos;s Technical Development department will enable synergies.
-                    </p>
+                    <InView threshold={0.25} >
+                        {({ inView, ref }) => (
+                            <div ref={ref} className="mx-auto my-5 md:w-1/2 md:h-96 h-72 overflow-hidden relative">
+                                <p className={`font-formulaRegular w-full md:text-lg whitespace-wrap absolute ${inView ? 'right-0 opacity-100' : 'right-[400px] opacity-0'} transition-all duration-1000`}>
+                                    In view of the major technological leaps that the series is making towards sustainability in 2026, we can speak of a new Formula 1. Formula 1 is transforming, and Audi wants to actively support this journey. A close link between our Formula 1 project and AUDI AG&apos;s Technical Development department will enable synergies.
+                                </p>
+                            </div>
+                        )}
+                    </InView>
                 </div>
                 {/* block 2 */}
                 <div className="my-3 p-5">
