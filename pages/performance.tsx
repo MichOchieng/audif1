e432b405890e6ac5a4d4d08ffe57b3f6381113ef
MichOchieng <MichOchieng@gmail.com>
2023-02-04 13:18:22 -0800

    First commit

---
+++ pages/performance.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const performance:NextPage = () => {
+  return (
+    <div>performance</div>
+  )
+}
+
+export default performance
---
+++ pages/sustainability.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const sustainability:NextPage = () => {
+  return (
+    <div>sustainability</div>
+  )
+}
+
+export default sustainability
