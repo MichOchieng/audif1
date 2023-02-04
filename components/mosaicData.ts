4fa87cd4cdc8625b65b9f4cc6b4298a194292160
MichOchieng <MichOchieng@gmail.com>
2023-02-04 13:47:04 -0800

    Added img imports

---
+++ components/mosaicData.ts
@@ -0,0 +1,23 @@
+import img1 from '../public/carImgs/A224464_large.jpg'
+import img2 from '../public/carImgs/A224461_large.jpg'
+import img3 from '../public/carImgs/A224476_large.jpg'
+import img4 from '../public/carImgs/A224478_large.jpg'
+import img5 from '../public/carImgs/A224477_large.jpg'
+
+export default [
+    {
+        bg: img1
+    },
+    {
+        bg: img2
+    },
+    {
+        bg: img3
+    },
+    {
+        bg: img4
+    },
+    {
+        bg: img5
+    },
+]
