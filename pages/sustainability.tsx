3415d1be3d0fe3a241151ea5c28314439f56c0ba
MichOchieng <MichOchieng@gmail.com>
2023-02-05 12:12:30 -0800

    Minor style changes to p tags

--- pages/sustainability.tsx
+++ pages/sustainability.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 import Image from 'next/image'
 import headerImg from '../public/carImgs/card1.jpg'
 import logo from '../public/audiLogo.png'
-import trackImg from '../public/carImgs/A226887_large.jpg'
+import rearImg from '../public/carImgs/A224465_large.jpg'
 import { InView } from 'react-intersection-observer'
 import { Tile } from '@/components'
+import Link from 'next/link'
 
 export default function Sustainability() {
     return (
@@ -39,39 +40,38 @@ export default function Sustainability() {
                             </div>
                         )}
                     </InView>
+                    <Link className='absolute m-2 md:m-5 hover:scale-105 smoothed top-0 left-0 h-10 w-24 md:h-16 md:w-32 flex items-center' href={'/'}>
+                        <Image className='object-contain' src={logo} alt='logo' />
+                    </Link>
                 </div>
                 {/* header */}
                 <div className="p-5 text-center md:my-5">
-                    <h1 className="font-formulaWide md:text-xl">Motorsport is an integral part of Audi&apos;s DNA</h1>
+                    <h1 className="font-formulaWide md:text-xl lg:text-3xl md:my-5">Motorsport is an integral part of Audi&apos;s DNA</h1>
                 </div>
                 {/* Block 1 */}
                 <div className="flex flex-col items-center md:flex-row w-full p-5">
                     {/* Tile */}
-                    <div className='flex flex-col items-center justify-center w-full md:h-96 h-72 border-t-8 border-r-8 border-[#D81919] rounded-tr-3xl rounded-bl-3xl text-white overflow-hidden z-10 smoothed'>
-                        <Image
-                            className='object-cover h-full z-0'
-                            src={headerImg}
-                            alt='bg'
-                        />
+                    <div className='flex flex-col items-center justify-center w-full md:h-96 h-72 border-t-8 border-r-8 border-[#D81919] rounded-tr-3xl rounded-bl-3xl text-white overflow-hidden z-10 smoothed bg-black'>
+                            <video loop autoPlay playsInline muted className='h-full object-cover' src="https://d2uism5v0u1o2x.cloudfront.net/audif1.mp4"/>
                     </div>
 
                 </div>
                 {/* block 2 */}
                 <div className="p-5">
                     {/* Text */}
-                    <p className="font-formulaRegular text-center">
+                    <p className="font-formulaRegular md:text-center p-text md:my-5">
                         The key to getting involved is the world&apos;s most popular racing series is the clear plan to become more sustainable and cost-efficient. The new technical rules, which will apply from 2026, focus on greater electrification and advanced sustainable fuel. In addition to the existing cap on costs for teams, a cost cap for power unit manufacturers will be introduced in 2023. In addition, Formula 1 has set itself the ambitious goal of being a carbon-neutral racing series by 2030.
                     </p>
                 </div>
                 {/* block 3 */}
                 <div className="flex flex-col items-center md:flex-row w-full p-5">
                     {/* Tile */}
-                    <Tile img={headerImg} />
+                    <Tile img={rearImg} />
                     {/* Text */}
                     <InView threshold={0.25} triggerOnce>
                         {({ inView, ref }) => (
-                            <div ref={ref} className="mx-auto w-full my-5 md:w-1/2 md:h-96 h-60 overflow-hidden relative">
-                                <p className={`font-formulaRegular w-full md:text-base whitespace-wrap absolute ${inView ? 'right-0 opacity-100' : 'right-[400px] opacity-0'} transition-all duration-1000`}>
+                            <div ref={ref} className="flex items-center mx-auto w-full my-5 md:w-1/2 md:h-96 h-80 overflow-hidden relative">
+                                <p className={`font-formulaRegular w-full p-text whitespace-wrap absolute ${inView ? 'right-0 opacity-100' : 'right-[400px] opacity-0'} transition-all duration-1000`}>
                                     Globally recognized, highly emotional, and technologically advanced, that&apos;s how Formula 1 has met Audi&apos;s exact requirements. The series holds races in all relevant markets of the brand. As a spectacular platform for the most demanding electrified racing cars in the world, it is of particular interest to car manufacturers. Audi is taking on the challenge of proving its “Vorsprung durch Technik” in this competitive environment.
                                 </p>
                             </div>
