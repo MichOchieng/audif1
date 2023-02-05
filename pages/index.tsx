e8516d3e55912a9d72df7bd6547919c6f26dea7d
MichOchieng <MichOchieng@gmail.com>
2023-02-05 12:56:12 -0800

    Updated header

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,19 @@ export default function Home() {
     <>
       <Head>
         <title>Audi F1 Team</title>
-        <meta name="description" content="Sauber to become Audi works F1 team from 2026" />
+        <meta name="description" content="Audi enters the premier class of motorsport" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/android-chrome-512x512.png" />
+        <link rel="android-chrome" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+        <link rel="android-chrome" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
       <main className=''>
-        <Parallax pages={4}>
-          <ParallaxLayer offset={0} sticky={{ start:0, end:0.2 }} speed={0.5}>
+        <Parallax pages={6}>
+          <ParallaxLayer offset={0} sticky={{ start: 0, end: 0.2 }} speed={0.5}>
             <Hero />
           </ParallaxLayer>
           <ParallaxLayer sticky={{ start: 1, end: 4 }} speed={0.5} >
@@ -26,8 +32,11 @@ export default function Home() {
           </ParallaxLayer>
           <ParallaxLayer sticky={{ start: 3.2, end: 4 }} speed={0.5} >
             <Card link='/performance' h1={cardData[2].h1} h2={cardData[2].h2} bg={cardData[2].bg} />
-            <Footer/>
           </ParallaxLayer>
+          <ParallaxLayer offset={5} factor={1} speed={0.5} >
+            <Footer />
+          </ParallaxLayer>
+
         </Parallax>
       </main>
     </>
